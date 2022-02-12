4b01bdf62606184bbab576207e469cf2df2ecd2c
Vincent Eliezer (Ram) <80811007+Vincenteliezer@users.noreply.github.com>
2022-02-13 01:48:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,15 +29,23 @@ const App = () => {
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             onKeyPress={search}
-
-            {weather.main && (
+        />
+         {weather.main && (
                 <div className='city-name'>
                     <h2>
-                        <span></span>
-                    </h2>                   
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2> 
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="info">
+                    <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                        </div>                  
                 </div>
             )}
-        />
    </div>
     );
 }
